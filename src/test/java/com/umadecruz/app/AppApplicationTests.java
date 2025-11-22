ddaa30b244a21c95ff0de4c5c652966d1ea96511
Abner Lucas Raymundo <abner.lucas@liax.tech>
2025-11-22 00:06:31 -0300

    Commit inicial

---
+++ src/test/java/com/umadecruz/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umadecruz.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
